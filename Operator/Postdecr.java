c0a9af54ba48520cc3cfc11e841aab93c8567178
Pratibha Verma <pratibhaverma7980@gmail.com>
2021-12-07 20:30:35 +0530

    ..

--- Operator/Postdecr.java
+++ Operator/Postdecr.java
@@ -1,9 +1,20 @@
 //POST DECREMENT
+class Increment{
+    void main(){
+        int a=5;
+        int b= a++;
+        System.out.print(a);
+        System.out.print(b);
+    }
 class Postdecr{
     public static void main (String[]args){
         int a=10;
         int b= a--;
         System.out.println(a);
          System.out.println(b);
+         Increment obj=new Increment();
+         obj.main();
     }
 }
+}
+
